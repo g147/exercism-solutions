eb0ea34803e29525e2213260177b0cb6ec57ece6
BEAST GLATISANT <26509147+g147@users.noreply.github.com>
2019-10-26 01:39:35 +0530

    solved some more

--- csharp/hello-world/HelloWorld.cs
+++ csharp/hello-world/HelloWorld.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-
-public static class HelloWorld
-{
-    public static string Hello()
-    {
-        throw new NotImplementedException("You need to implement this function.");
+public static class HelloWorld{
+    public static string Hello(){
+        return "Hello, World!";
     }
 }
