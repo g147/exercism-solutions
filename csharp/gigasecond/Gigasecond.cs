7dade201c0dbfc36ad001db161e009246a5dc65b
BEAST GLATISANT <26509147+g147@users.noreply.github.com>
2019-11-02 23:47:50 +0530

    solved some more

---
+++ csharp/gigasecond/Gigasecond.cs
@@ -0,0 +1,6 @@
+using System;
+public static class Gigasecond{
+    public static DateTime Add(DateTime moment){
+        return moment.AddSeconds(1E9);
+    }
+}
---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+public static class TwoFer{
+    public static string Speak(){
+        return "One for you, one for me.";
+    }
+    public static string Speak(string x){
+        return "One for "+x+", one for me.";
+    }
+}
