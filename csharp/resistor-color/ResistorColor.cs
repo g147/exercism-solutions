d4e20bbeafb9bed3f2ab511058f5bf79b20cb88b
BEAST GLATISANT <26509147+g147@users.noreply.github.com>
2019-11-06 12:38:49 +0530

    solved some more

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public static class ResistorColor{
+    public static int ColorCode(string color){
+        String[] a = new String[10];
+        a = Colors();
+        int pos = Array.IndexOf(a, color);
+        return pos;
+    }
+
+    public static string[] Colors(){
+        String[] arr = new String[] {"black","brown","red","orange","yellow","green","blue","violet","grey","white"};
+        return arr;
+    }
+}
